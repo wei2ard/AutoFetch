d7579dd717511a2293688ffd5566d730379738d5
WE1ZARD <weizard.inc@gmail.com>
2024-11-14 12:16:01 +0800

    fix jq syntax error

--- fetch.sh
+++ fetch.sh
@@ -15,10 +15,10 @@ RESET='\e[0m'
 USR=(       $(cat repos.json | jq -r '.[].user'))
 REPO=(      $(cat repos.json | jq -r '.[].repo'))
 FILE=(      $(cat repos.json | jq -r '.[].filename'))
-ASSET_ID=(  $(cat repos.json | jq -r '.[].asset_id'))
+PRE=(       $(cat repos.json | jq -r '.[].pre'))
 NUM=(       $(cat repos.json | jq -r '.[].num'))
 TYPE=(      $(cat repos.json | jq -r '.[].type'))
-DIR=(      $(cat repos.json | jq -r '.[].dir'))
+DIR=(       $(cat repos.json | jq -r '.[].dir'))
 
 function Fetch() {
   params=($@)
@@ -27,27 +27,27 @@ function Fetch() {
     user=${USR[$param]}
     repo=${REPO[$param]}
     file=${FILE[$param]}
-    asset_id=${ASSET_ID[$param]}
+    pre=${PRE[$param]}
     num=${NUM[$param]}
     type=${TYPE[$param]}
     dir=${DIR[$param]}
     
     if [[ ${user} == WE1ZARD ]]; then # release
         curl -sL -H "Authorization: token $TOKEN" -sL "$API/${user}/${repo}/releases/latest" | jq ".assets" | jq ".[${num}].url" | xargs -I {} curl -sL {} -H "Authorization: token $TOKEN" -H 'Accept:application/octet-stream' -o ${file}${type}
-        # echo -e ${user}
-        if [[ ${asset_id} > 0 ]]; then # pre-release 
-            curl -sL -H "Accept: application/octet-stream" -H "Authorization: Bearer $TOKEN" "$API/${user}/${repo}/releases/assets/${asset_id}" -o ${file}"-pre"${type}
-            #echo -e " ${file}-pre${type}"
+        echo -e " ${file}${type}"
+        if [[ ${pre} > 0 ]]; then # pre-release 
+            curl -sL -H "Authorization: token $TOKEN" -sL "$API/${user}/${repo}/releases" | jq '.[]' | jq 'select(.prerelease)' | jq -r '.assets.[].url' | xargs -I {} curl -sL -H "Accept: application/octet-stream" -H "Authorization: Bearer $TOKEN" {} -o ${file}"-pre"${type}; 
+            echo -e " ${file}-pre${type}"
         fi
     else
         curl -sL "$API/${user}/${repo}/releases" | jq ".[0].assets" | jq ".[${num}].browser_download_url" | xargs -I {} curl -sL {} -o ${file}${type}
-        #echo -e ${user}
+        echo -e ${user} ${file}${type}
     fi
 
     if [[ "${dir}" != 0 ]]; then
       if [ ! -d "${dir}" ]; then
         mkdir -p "${dir}"
-        #echo -e " make dir ${dir}"
+        echo -e " make dir ${dir}"
       fi
       mv ${file}${type} ${dir}
       zip -rq "${file}.zip" ${dir}
